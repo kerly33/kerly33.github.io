a07d748d16e07cc23c585830435bc8044a28e63d
kerly <kerlyvictoria21@gmail.com>
2022-03-22 16:20:19 +0100

    main menu

--- src/app/index/index.component.ts
+++ src/app/index/index.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-index',
@@ -7,9 +8,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class IndexComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private router: Router,
+  ) { }
 
   ngOnInit(): void {
+  }
+
+  inicio() {
+    this.router.navigate(['']);
+  }
+
+  comentarios() {
+    this.router.navigate(['']);
+  }
+
+  contacto() {
+    this.router.navigate(['']);
   }
 
 }
